cae8e85b04f78b44e3bd660b0d8be1a3a61d229e
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2020-04-07 09:21:07 -0700

    Add index.js with vscode settings to run formatOnSave

---
+++ index.js
@@ -0,0 +1,6 @@
+const array = [
+  "although this project uses prettier@2",
+  "trailingComma won't be added to this array",
+  "as prettier config uses prettier@1 defaults",
+  "using prettier-v1-config"
+];
